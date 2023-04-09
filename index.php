1c450bd7f26a00438c20a407a48e405c16c24321
yz-baskoy <yziyabaskoy@gmail.com>
2023-04-10 01:52:04 +0300

    Remove unused import

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 
 require 'vendor/autoload.php';
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 $app = AppFactory::create();
