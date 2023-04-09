dcf17a13495a8c5eb7d1919b9b9cdd36c908e77e
yz-baskoy <yziyabaskoy@gmail.com>
2023-04-10 01:51:28 +0300

    Remove unused import

--- src/routes/users.php
+++ src/routes/users.php
@@ -4,9 +4,6 @@ require 'vendor/autoload.php';
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
-
-//$app = AppFactory::create();
 
 // User registration
 $app->post('/register', function (Request $request, Response $response) {
